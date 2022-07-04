fe2bb3ccdecdb7d56531a0183d99ecae59368264
Cece <cecesanclemente@gmail.com>
2022-07-04 17:28:27 -0500

    move AllPlayersContainer to it's own file

---
+++ src/Components/AllPlayersContainer.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+export default function AllPlayersContainer(props) {
+  return (
+    <section className="all-players_section">
+      {props.allPlayers?.map((p) => {
+        return (
+          <section className="each-player_section" key={p.id}>
+            <div className="row">
+              <div className="delete-player_btn">
+                <button
+                  onClick={() => props.deletePlayer(p.id)}
+                  style={{
+                    backgroundColor: p.color,
+                    borderColor: p.color,
+                    color: "white",
+                  }}
+                >
+                  X
+                </button>
+              </div>
+              <div className="player-name_container">
+                <div
+                  className="player-name"
+                  style={{ backgroundColor: p.color }}
+                >
+                  {p.playerName}
+                </div>
+              </div>
+              <div className="score_container">
+                <div className="score-input_container">
+                  <form onSubmit={(e) => props.handleScoreSubmit(e, p)}>
+                    <input
+                      className="score_input"
+                      style={{ borderColor: p.color, color: p.color }}
+                      type="number"
+                      name="newScore"
+                      value={p.newScore}
+                      onChange={(e) => props.handleNewScore(e, p)}
+                    ></input>
+                    <button
+                      type="submit"
+                      className="add-score_btn"
+                      style={{
+                        backgroundColor: p.color,
+                        borderColor: p.color,
+                      }}
+                    >
+                      +
+                    </button>
+                  </form>
+                </div>
+                <div className="total-score_container">
+                  <span className="bold small">Score</span>
+                  <span className="total-score bold">{p.totalScore}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="scores_container">
+              <div>
+                <span className="bold small" style={{ marginRight: "8px" }}>
+                  Scores:
+                </span>
+                <span className="scores-arr">{p.scores.join(", ")}</span>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </section>
+  );
+}
--- src/Components/Players.js
+++ src/Components/Players.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AddPlayersContainer from "./AddPlayersContainer";
+import AllPlayersContainer from "./AllPlayersContainer";
 
 export default function Players() {
   const [newPlayer, setNewPlayer] = useState({
@@ -112,7 +113,7 @@ export default function Players() {
           >
             {winner.totalScore}
           </span>
-          🥳
+          🎉
         </h2>
       );
   }
@@ -124,73 +125,12 @@ export default function Players() {
         newPlayer={newPlayer}
         handleChange={handleChange}
       />
-      <section className="all-players_section">
-        {allPlayers?.map((p) => {
-          return (
-            <section className="each-player_section" key={p.id}>
-              <div className="row">
-                <div className="delete-player_btn">
-                  <button
-                    onClick={() => deletePlayer(p.id)}
-                    style={{
-                      backgroundColor: p.color,
-                      borderColor: p.color,
-                      color: "white",
-                    }}
-                  >
-                    X
-                  </button>
-                </div>
-                <div className="player-name_container">
-                  <div
-                    className="player-name"
-                    style={{ backgroundColor: p.color }}
-                  >
-                    {p.playerName}
-                  </div>
-                </div>
-                <div className="score_container">
-                  <div className="score-input_container">
-                    <form onSubmit={(e) => handleScoreSubmit(e, p)}>
-                      <input
-                        className="score_input"
-                        style={{ borderColor: p.color, color: p.color }}
-                        type="number"
-                        name="newScore"
-                        value={p.newScore}
-                        onChange={(e) => handleNewScore(e, p)}
-                      ></input>
-                      <button
-                        type="submit"
-                        className="add-score_btn"
-                        style={{
-                          backgroundColor: p.color,
-                          borderColor: p.color,
-                        }}
-                      >
-                        +
-                      </button>
-                    </form>
-                  </div>
-                  <div className="total-score_container">
-                    <span className="bold small">Score</span>
-                    <span className="total-score bold">{p.totalScore}</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="scores_container">
-                <div>
-                  <span className="bold small" style={{ marginRight: "8px" }}>
-                    Scores:
-                  </span>
-                  <span className="scores-arr">{p.scores.join(", ")}</span>
-                </div>
-              </div>
-            </section>
-          );
-        })}
-      </section>
+      <AllPlayersContainer
+        allPlayers={allPlayers}
+        deletePlayer={deletePlayer}
+        handleScoreSubmit={handleScoreSubmit}
+        handleNewScore={handleNewScore}
+      />
 
       <section className="winning-player_section">
         <h3>Who's Winning?</h3>
